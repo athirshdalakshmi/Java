a2bfe25663319af06a55d3a610a988adc46f2bcf
athirshdalakshmi <98007532+athirshdalakshmi@users.noreply.github.com>
2023-09-04 10:17:05 +0530

    Day 3

---
+++ Ex 3.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    int sum = 0;
+    int n = 1000;
+    for (int i = n; i >= 1; --i) 
+    {
+      sum += i;   
+    }
+    System.out.println("Sum = " + sum);
+  }
+}
